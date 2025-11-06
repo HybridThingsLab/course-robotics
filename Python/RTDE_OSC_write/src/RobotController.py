d563bf060bdfb837d8bca510d3bf22caf277934d
Andreas Muxel <login@andreasmuxel.com>
2025-11-06 11:06:43 +0100

    update 3D view TD

--- Python/RTDE_OSC_write/src/RobotController.py
+++ Python/RTDE_OSC_write/src/RobotController.py
@@ -34,8 +34,8 @@ class RobotController(Thread):
         self.stopRequest = False
 
         #prepare robot connection
-        self.robotIP = "127.0.0.1"
-        #self.robotIP = "192.168.188.29"
+        #self.robotIP = "127.0.0.1"
+        self.robotIP = "192.168.188.29"
         self.readFreq = 500
         
 
